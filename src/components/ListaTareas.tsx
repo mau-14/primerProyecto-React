047faaf3c48299b0943880fd8b79acd38c0fd7a7
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2025-01-12 11:46:41 +0100

    añadido exports en todo

--- src/components/ListaTareas.tsx
+++ src/components/ListaTareas.tsx
@@ -1,13 +1,15 @@
+import { Tarea } from "./Tarea"
+
 type ListaTareas= {
   listaTareas: string[]
   borrarTarea: (index:number) => void
 }
 
-const ListaTareas = ({listaTareas, borrarTarea}: ListaTareas) => {
+export const ListaTareas = ({listaTareas, borrarTarea}: ListaTareas) => {
   return (
     <div className='taskList'>
-      {listaTareas.map(task,index) => (
-
+      {listaTareas.map(tarea,index) => (
+        <Tarea key={index} tarea={tarea} borrarTarea={() => borrarTarea}></Tarea>
       )}
     </div>
   )
--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { ListaTareas } from "./ListaTareas"
 
 export const TodoApp = () => {
 
@@ -7,7 +8,13 @@ export const TodoApp = () => {
   const [listaTareas, setListaTareas] = useState<string[]>([])
 
   const handleAddTask = () => {
-    
+    if(nuevaTarea.trim() === '') return
+    setListaTareas(tareasAnteriores => [...tareasAnteriores, nuevaTarea])
+    setNuevaTarea('')
+  }
+
+  const handleBorrarTarea = (index: number) => {
+    setListaTareas(tareas => tareas.filter((_,i) => i !== index))
   }
 
   return (
@@ -17,6 +24,7 @@ export const TodoApp = () => {
         <input type="text" value={nuevaTarea} onChange={(e) => setNuevaTarea(e.target.value)} placeholder="Nueva Tarea"/>
         <button onClick={handleAddTask}></button>
       </div>
+      <ListaTareas listaTareas={listaTareas} borrarTarea={handleBorrarTarea}></ListaTareas>
     </div>
   )
 }
