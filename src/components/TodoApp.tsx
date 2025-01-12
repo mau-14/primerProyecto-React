d6fcf15d3b30b4249192a58e44dd40643b9c590e
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2025-01-12 11:31:22 +0100

    agregando typescript

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -2,12 +2,20 @@ import { useState } from "react"
 
 export const TodoApp = () => {
 
-  const [nuevaTarea, setNuevaTarea] = useState('')
+  const [nuevaTarea, setNuevaTarea] = useState<string>('')
+
+  const [listaTareas, setListaTareas] = useState<string[]>([])
+
+  const handleAddTask = () => {
+    
+  }
+
   return (
     <div>
       <h1>Lista de Tareas</h1>
       <div>
         <input type="text" value={nuevaTarea} onChange={(e) => setNuevaTarea(e.target.value)} placeholder="Nueva Tarea"/>
+        <button onClick={handleAddTask}></button>
       </div>
     </div>
   )
